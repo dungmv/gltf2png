382ee51253b0159ec2786a54e5d96f2a2f6b4d3c
dungmv <maivandung91@gmail.com>
2023-06-26 00:10:40 +0700

    bootstrap server

--- index.js
+++ index.js
@@ -1,22 +1,19 @@
 import express from 'express'
+import multer from 'multer'
 import { WebGLRenderingContext } from 'gl'
-import { Scene, AmbientLight, PerspectiveCamera, WebGLRenderer, Texture, TextureLoader } from 'three'
+import { Scene, AmbientLight, PerspectiveCamera, WebGLRenderer, TextureLoader } from 'three'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { createCanvas } from './canvasGL/index.js'
 import { JSDOM } from 'jsdom'
 import { Blob } from 'node:buffer'
-import multer from 'multer'
-import { readFile, readFileSync } from 'node:fs'
-
-const width = 512
-const height = 512;
+import { readFileSync } from 'node:fs'
 
 const { window: JSDOMWindow } = new JSDOM(
 	'<!DOCTYPE html><html><body></body></html>',
 	{
-	  resources: 'usable',
-	//   needed for `requestAnimationFrame`
-	//   pretendToBeVisual: true,
+		resources: 'usable',
+		// needed for `requestAnimationFrame`
+		// pretendToBeVisual: true,
 	}
 );
 
@@ -27,60 +24,72 @@ global.self.URL = URL;
 global.Blob = Blob;
 
 global.self.URL.createObjectURL = (blob) => {
-    return new Promise(resolve => {
-        blob.arrayBuffer().then(buffer => {
-            const base64 = new Buffer.from(buffer).toString('base64');
-            const completedURI = `data:image/png;base64,` + base64;
-            resolve(completedURI);
-        });
-    })
+	return new Promise(resolve => {
+		blob.arrayBuffer().then(buffer => {
+			const base64 = new Buffer.from(buffer).toString('base64');
+			const completedURI = `data:image/png;base64,${base64}`;
+			resolve(completedURI);
+		});
+	})
 };
 
-const app = express();
+const bootstrap = async (port) => {
+	const bgTexBuf = readFileSync('./asset/rectangle_10657.png')
+	const base64 = new Buffer.from(bgTexBuf.buffer).toString('base64');
+	const completedURI = `data:image/png;base64,${base64}`;
+	const background = await new TextureLoader().loadAsync(completedURI);
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage });
+	const app = express();
+	
+	const storage = multer.memoryStorage();
+	const upload = multer({ storage });
 
-app.post('/', upload.single('file'),  async(req, res) => {
-	const gltfLoader = new GLTFLoader();
-	const gltf = await gltfLoader.parseAsync(req.file.buffer.buffer, '');
+	app.post('/png', upload.single('file'), async (req, res) => {
+		const width = parseInt(req.query.w) || 512
+		const height = parseInt(req.query.h) || 512
 
-	const scene = new Scene();
+		const canvas = createCanvas(width, height);
+	
+		const renderer = new WebGLRenderer({
+			antialias: true,
+			preserveDrawingBuffer: true,
+			canvas: canvas,
+		});
 
-	const d = readFileSync('./asset/rectangle_10657.png')
-	const base64 = new Buffer.from(d.buffer).toString('base64');
-	const completedURI = `data:image/png;base64,` + base64;
-	scene.background = await new TextureLoader().loadAsync(completedURI);
+		const gltfLoader = new GLTFLoader();
+		const gltf = await gltfLoader.parseAsync(req.file.buffer.buffer, '');
 	
-	scene.add(new AmbientLight(0xFFFFFF));
-	gltf.scene.position.set(0, 0, 0)
-	scene.add(gltf.scene)
+		const scene = new Scene();
 	
-	const camera = new PerspectiveCamera(35, width / height, 1, 1000);
-	camera.position.set(5, 5, 10)
+		scene.background = background;
+		scene.fog = null;
 	
-	camera.lookAt(0, 0, 0);
+		scene.add(new AmbientLight(0xFFFFFF));
+		gltf.scene.position.set(0, 0, 0)
+		scene.add(gltf.scene)
 	
-	const canvas = createCanvas(width, height);
-	
-	const renderer = new WebGLRenderer({
-		antialias: true,
-		preserveDrawingBuffer: true,
-		canvas: canvas,
+		const camera = new PerspectiveCamera(35, width / height, 1, 1000);
+		camera.position.set(0, 5, 15)
+		camera.lookAt(0, 2, 0)
+
+		renderer.setSize(width, height);
+		renderer.render(scene, camera)
+		canvas.createPNGStream('image/png').pipe(res);
 	});
 	
-	// renderer.setSize(width, height);
+	app.use((err, req, res, next) => {
+		console.error(err.stack)
+		res.status(500).send('Something broke!')
+	})
 	
-	renderer.render(scene, camera)
-	
-	canvas.createPNGStream('image/png').pipe(res);
-});
+	app.listen(port, () => {
+		console.log(`Server is running on port ${port}`);
+	});	
+};
 
-app.use((err, req, res, next) => {
-	console.error(err.stack)
-	res.status(500).send('Something broke!')
+bootstrap(3000).then(
+	console.log('server is ready')
+).catch(e => {
+	console.error('can\'t start server with error', e)
 })
 
-app.listen(3000, () => {
-	console.log('Server is running on port 3000');
-});
